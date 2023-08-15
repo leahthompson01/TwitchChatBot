77e4cc54ca9a2d4a93406b0bd5d7e0ec8521dd5f
leahthompson01 <leahmthompson03@gmail.com>
2023-08-14 23:36:42 -0600

    current progress on adding back previous functionality to the bot

--- refactoredApp.js
+++ refactoredApp.js
@@ -1,12 +1,24 @@
 require("dotenv").config();
 
-
 const tmi = require("tmi.js");
-const fs = require('fs-extra');
-const path = require('path');
-const data = fs.readJsonSync(path.join(__dirname, 'chatbotCommands.json'));
-const aliasData = fs.readJsonSync(path.join(__dirname, 'aliases.json'));
-console.log("alias data ", aliasData);
+const fs = require("fs-extra");
+const path = require("path");
+const data = fs.readJsonSync(path.join(__dirname, "chatbotCommands.json"));
+const aliasData = fs.readJsonSync(path.join(__dirname, "aliases.json"));
+const faveUsers = {
+  earend: false,
+  valkeryias: false,
+  theyagich: false,
+  theidofalan: false,
+  schipthatcode: false,
+  madeleinepatience: false,
+  grimybadger: false,
+  jddoesdev: false,
+  seaninretro: false,
+  codingwithstrangers: false,
+  aza_app: false,
+  grandbuh: false,
+};
 
 const client = new tmi.client({
   options: { debug: true },
@@ -37,55 +49,55 @@ client.on("message", (channel, tags, message, self) => {
   // const canHandle = map.has(messageArr[0])
   // if (canHandle) Map.get
 
-
   if (messageArr.length >= 2 && messageArr[0] === "!so") {
     username = messageArr[1].replace("@", "");
     const keysArr = Object.keys(data);
     let noCustom = true;
-    for( let i=0; i<keysArr.length; i++ ){
-      if(data[keysArr[i]]["username"] === username){
+    for (let i = 0; i < keysArr.length; i++) {
+      if (data[keysArr[i]]["username"] === username) {
         const shoutoutsArr = data[keysArr[i]]["shoutouts"];
         let randomIndex = Math.floor(shoutoutsArr.length * Math.random());
         client.say(channel, shoutoutsArr[randomIndex]);
         noCustom = false;
       }
-      }
-    if(noCustom){
-      client.say(channel, `${
-        messageArr[1]
-      } is an amazing streamer! Go follow them at https://www.twitch.tv/${messageArr[1].replace(
-        "@",
-        ""
-      )}`);
     }
-
+    if (noCustom) {
+      client.say(
+        channel,
+        `${
+          messageArr[1]
+        } is an amazing streamer! Go follow them at https://www.twitch.tv/${messageArr[1].replace(
+          "@",
+          ""
+        )}`
+      );
+    }
   }
-  if(aliasData[messageLC]){
+  if (aliasData[messageLC]) {
     messageLC = aliasData[messageLC];
   }
-  if(data[messageLC]){
+  if (data[messageLC]) {
     const shoutoutsArr = data[messageLC]["shoutouts"];
     let randomIndex = Math.floor(shoutoutsArr.length * Math.random());
 
     client.say(channel, shoutoutsArr[randomIndex]);
   }
-  if(messageArr[0] === "!addcommand" && (tags?.badges?.["moderator"] || tags?.badges?.["broadcaster"])){
+  // need to add a check that sees if the shoutout already exists, and if so it adds to the shoutouts
+  if (
+    messageArr[0] === "!addcommand" &&
+    (tags?.badges?.["moderator"] || tags?.badges?.["broadcaster"])
+  ) {
     const newCommand = {
-      "shoutouts": [messageArr.slice(2).join(" ")]
-
-    }
+      shoutouts: [messageArr.slice(2).join(" ")],
+    };
     data[`!${messageArr[1]}`] = newCommand;
     console.log(data);
-    fs.writeFileSync('chatbotCommands.json', JSON.stringify(data));
-
+    fs.writeFileSync("chatbotCommands.json", JSON.stringify(data, null, 1));
   }
-
 
   // !addAlias shoutout alias
   // !addCommand shoutout content
   // !addso username shoutout content
-
-  
 
   //   console.log(message);
 });
